58bcadada198c5a7b2c1fdbf9ba2d6a53ebaab93
Cathrin Whitmer <catwhitmer@gmail.com>
2020-05-14 19:05:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ function leashDog(dogName, dogBreed) {
   console.log(`Leash ${dogName} the ${dogBreed}`);
 }
 
-
+function wakeDog(dogName, dogBreed) {
   console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+  
+  
   console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
   console.log(`Walk home with ${dogName} the ${dogBreed}`);
   console.log(`Unleash ${dogName} the ${dogBreed}`);
