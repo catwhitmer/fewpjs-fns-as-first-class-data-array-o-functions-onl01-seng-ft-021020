5d104c6e41593bd061e483810e71c4da02be6836
Cathrin Whitmer <catwhitmer@gmail.com>
2020-05-14 19:10:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,11 +21,12 @@ function throwFrisbee(dogName, dogBreed) {
   
 function walkHome(dogName, dogBreed) {
   `Walk home with ${dogName} the ${dogBreed}`;
-  
+  console.log
 }
   
 function unleashDog(dogName, dogBreed) {
   console.log`Unleash ${dogName} the ${dogBreed}`;
+  
 }
 
 let routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog];
