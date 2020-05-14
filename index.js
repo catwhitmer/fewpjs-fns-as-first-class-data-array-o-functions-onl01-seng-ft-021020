404068ffa6f7bf0c5d497a8acde227032faf3db0
Cathrin Whitmer <catwhitmer@gmail.com>
2020-05-14 19:06:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ function wakeDog(dogName, dogBreed) {
   console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
 }
   
-  function wakeDog(dogName, dogBreed) {
+function wakeDog(dogName, dogBreed) {
   console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
 }
   
-  function wakeDog(dogName, dogBreed) {
+function wakeDog(dogName, dogBreed) {
   console.log(`Walk home with ${dogName} the ${dogBreed}`);
 }
   
-  function wakeDog(dogName, dogBreed) {
+function wakeDog(dogName, dogBreed) {
   console.log(`Unleash ${dogName} the ${dogBreed}`);
 }
 
