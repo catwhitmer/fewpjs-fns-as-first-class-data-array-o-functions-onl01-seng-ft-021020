96e497fa17970670365b017f0844f268618f9609
Cathrin Whitmer <catwhitmer@gmail.com>
2020-05-14 18:58:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ function wakeDog(dogName, dogBreed) {
 
 
 
+console.log("Leash Byron the poodle");
+console.log("Walk to the park with Byron the poodle");
+console.log("Throw the frisbee for Byron the poodle");
+console.log("Walk home with Byron the poodle");
+console.log("Unleash Byron the poodle");
